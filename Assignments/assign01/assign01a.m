2015d472e18c2107785b9110d49a392808341db6
drewdunn14 <48300414+drewdunn14@users.noreply.github.com>
2020-04-04 19:08:10 -0500

    Add files via upload

---
+++ Assignments/assign01/assign01a.m
@@ -0,0 +1,12 @@
+%{
+Drew Dunn
+assign01a.m
+January 24, 2020
+This is a simple program which prints a message to
+welcome you to Auburn.
+%}
+clc, clear all
+format compact
+
+disp( ['Welcome to COMP-', num2str(60 * (sqrt(400.0)))] );
+disp( 'War Eagle!' )
